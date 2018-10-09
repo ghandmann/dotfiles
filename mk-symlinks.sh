bbb034e000acfebe9dbc697ecdd54532bcb777fd
Sven Eppler <sven.eppler@sodgeit.de>
2018-10-09 09:22:56 +0200

    Added ackrc

--- mk-symlinks.sh
+++ mk-symlinks.sh
@@ -4,6 +4,7 @@ ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/screenrc ~/.screenrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/ackrc ~/.ackrc
 
 if [ ! -d ~/.vim/ ]
 then
