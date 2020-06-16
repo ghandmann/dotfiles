81fa8b598b90ad285bc09c8d4ba5be86ffa2af37
Sven Eppler <sven.eppler@sodgeit.de>
2020-06-16 22:59:39 +0200

    Added liquidprompt

--- mk-symlinks.sh
+++ mk-symlinks.sh
@@ -5,6 +5,7 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/screenrc ~/.screenrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/ackrc ~/.ackrc
+ln -s ~/dotfiles/liquidpromptrc ~/.liquidpromptrc
 
 if [ ! -d ~/.vim/ ]
 then
